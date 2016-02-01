fa925fae6d947b7419adc92cb917ac4c06b1248d
Tjeerd Bijlsma <tjeerd@ibuildings.nl>
2016-02-01 13:58:23 +0100

    Add some basic smoke tests for Voot url's

--- tests/AppBundle/Controller/VootControllerTest.php
+++ tests/AppBundle/Controller/VootControllerTest.php
@@ -10,8 +10,26 @@ class VootControllerTest extends WebTestCase
     {
         $client = static::createClient();
 
-        $client->request('GET', '/');
+        $client->request('GET', '/voot');
 
         $this->assertEquals(404, $client->getResponse()->getStatusCode());
+    }
+
+    public function testSmokeGroupsIndex()
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/voot/user/1/groups');
+
+        $this->assertEquals(401, $client->getResponse()->getStatusCode());
+    }
+
+    public function testSmokeGroupIndex()
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/voot/user/1/groups/1');
+
+        $this->assertEquals(401, $client->getResponse()->getStatusCode());
     }
 }

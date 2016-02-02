393456fb7c2df603355a9afa1316e5e849075700
Tjeerd Bijlsma <tjeerd@ibuildings.nl>
2016-02-02 16:21:13 +0100

    Make sure grouped groups are only displayed if they contain any groups

--- src/AppBundle/Controller/IndexController.php
+++ src/AppBundle/Controller/IndexController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Model\Membership;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -15,6 +16,49 @@ class IndexController extends Controller
      */
     public function indexAction()
     {
-        return $this->render(':index:index.html.twig');
+        $user = $this->getUser();
+
+        $apiClient = $this->get('app.api_client');
+
+        /** @var Membership[] $myGroups */
+        $myGroups = $apiClient->findUserMemberships(7);//$user->getId());
+
+        $ownerGrouphubGroups = $adminGrouphubGroups = $memberGrouphubGroups = [];
+        $ownerOtherGroups = $adminOtherGroups = $memberOtherGroups = [];
+        foreach ($myGroups as $group) {
+            if ($group->getGroup()->getType() === 'grouphub' && $group->getRole() == 'owner') {
+                $ownerGrouphubGroups[$group->getGroup()->getId()] = $group->getGroup();
+            }
+
+            if ($group->getGroup()->getType() === 'grouphub' && $group->getRole() == 'admin') {
+                $adminGrouphubGroups[$group->getGroup()->getId()] = $group->getGroup();
+            }
+
+            if ($group->getGroup()->getType() === 'grouphub' && $group->getRole() == 'member') {
+                $memberGrouphubGroups[$group->getGroup()->getId()] = $group->getGroup();
+            }
+
+            if ($group->getGroup()->getType() !== 'grouphub' && $group->getRole() == 'owner') {
+                $ownerOtherGroups[$group->getGroup()->getId()] = $group->getGroup();
+            }
+
+            if ($group->getGroup()->getType() !== 'grouphub' && $group->getRole() == 'admin') {
+                $adminOtherGroups[$group->getGroup()->getId()] = $group->getGroup();
+            }
+
+            if ($group->getGroup()->getType() !== 'grouphub' && $group->getRole() == 'member') {
+                $memberOtherGroups[$group->getGroup()->getId()] = $group->getGroup();
+            }
+        }
+
+        return $this->render(':index:index.html.twig', [
+            'ownerGrouphubGroups'  => $ownerGrouphubGroups,
+            'adminGrouphubGroups'  => $adminGrouphubGroups,
+            'memberGrouphubGroups' => $memberGrouphubGroups,
+            'ownerOtherGroups'     => $ownerOtherGroups,
+            'adminOtherGroups'     => $adminOtherGroups,
+            'memberOtherGroups'    => $memberOtherGroups,
+            'groups'               => $apiClient->findGroups(),
+        ]);
     }
 }

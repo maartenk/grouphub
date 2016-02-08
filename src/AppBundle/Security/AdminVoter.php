e60cfb46748d477a108af39a58a195cfd041b3d4
Tjeerd Bijlsma <tjeerd@ibuildings.nl>
2016-02-08 13:53:45 +0100

    Added security check to determine whether or not a user is allowed to add semi-formal groups

---
+++ src/AppBundle/Security/AdminVoter.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace AppBundle\Security;
+
+use AppBundle\Manager\GroupManager;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * Class AdminVoter
+ */
+class AdminVoter extends Voter
+{
+    /**
+     * @var GroupManager
+     */
+    private $groupManager;
+
+    /**
+     * @param GroupManager $groupManager
+     */
+    public function __construct(GroupManager $groupManager)
+    {
+        $this->groupManager = $groupManager;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function supports($attribute, $subject)
+    {
+        return $attribute === 'ROLE_ADMIN';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function voteOnAttribute($attribute, $subject, TokenInterface $token)
+    {
+        $user = $token->getUser();
+
+        if (!$user instanceof UserInterface) {
+            return false;
+        }
+
+        $groups = $this->groupManager->getMyGroups($user);
+
+        if (empty($groups['other'])) {
+            return false;
+        }
+
+        foreach ($groups['other'] as $otherGroups) {
+            if (in_array(1, array_keys($otherGroups), true)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}

3b28d3735a41e9f801284fc504304381a5db9cbd
Tjeerd Bijlsma <tjeerd@ibuildings.nl>
2016-02-12 18:41:22 +0100

    Simplified permission check

--- src/AppBundle/Security/GroupVoter.php
+++ src/AppBundle/Security/GroupVoter.php
@@ -53,18 +53,13 @@ class GroupVoter extends Voter
             return true;
         }
 
-        // @todo: Not very efficient - load all??
-        $memberships = $this->groupManager->findGroupMemberships($subject->getId());
+        $membership = $this->groupManager->findUserMembershipOfGroup($user->getId(), $subject->getId());
 
-        foreach ($memberships as $membership) {
-            if ($membership->getRole() !== Membership::ROLE_ADMIN) {
-                continue;
-            }
+        if (!$membership) {
+            return false;
+        }
 
-            if ($membership->getUser()->getId() !== $user->getId()) {
-                continue;
-            }
-
+        if ($membership->getRole() === Membership::ROLE_ADMIN) {
             return true;
         }
 
